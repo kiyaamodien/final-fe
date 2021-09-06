d27b48efa30911cfd3d6c282147f7ccecf8d38d9
kiyaamodien <kiyaamudienkhan@gmail.com>
2021-09-06 16:24:52 +0200

    update

--- rooms.js
+++ rooms.js
@@ -1,3 +1,6 @@
+let rooms = [];
+let carts = [];
+
 const user = JSON.parse(localStorage.getItem("user"));
 console.log(user);
 
@@ -11,13 +14,21 @@ function getRooms() {
       ).innerHTML = `Hi there ${user.username}`;
       res["data"].forEach((room) => {
         document.querySelector("#rooms").innerHTML += `
+        <button onclick="addToCart(${room.room_id})" class="room-s">Click on Room to add to Cart
           <div class="room-card">
+          <select id="framework">
+    <option value="1">1d</option>
+    <option value="2">2d</option>
+    <option value="3">3d.</option>
+    <option value="4">4d.</option>
+</select>
             <h3 class="room-name">${room[1]}</h3>
             <p class="room-type">${room[2]}</p>
-            <p class="room-image">${room[3]}
+            <img class="room-image" src='${room[3]}'>/
             <p class="price">${room[4]}</p>
-            <p class="room-view">${room[5]}</p>
+            <p class="room-view">View/${room[5]}</p>
           </div>
+          </button>
         `;
       });
     });
@@ -63,3 +74,52 @@ getRooms();
 //       `;
 //   });
 // }
+
+function searchedForRooms() {
+  let searchTerm = document.querySelector("#searchTerm").Value;
+  console.log(searchTerm);
+
+  let searchedRooms = rooms.filter((room) =>
+    room[1].toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  console.log(searchedRooms);
+  getRooms(searchedRooms);
+
+  if (searchedRooms.length == 0) {
+    document.querySelector("#rooms").innerHTML =
+      "<h2>Unable to find room or, room unavailable<h2>";
+  } else fetRooms(searchedRooms);
+}
+
+function logout() {
+  localStorage.clear();
+  window.location = "./index.html";
+}
+
+function toggleCreaateRoomModal() {
+  document.querySelector("#create-room-modal").classList.toggle("active");
+}
+
+function renderBooking(cartItems) {
+  let bookingContainer = document.querySelector("#booking");
+  bookingContainer.innerHTML = "";
+  if (bookingItems.length > 0) {
+    bookingItems.map((bookingItem) => {
+      bookingContianer.innerHTML += `
+      <div class="room">
+  <img src="${bookingItem.image}" class="room-image">
+          <div class="room-content"></div>
+            <h3 class="room-name">${room[1]}</h3>
+            <p class="room-type">${room[2]}</p>
+            <img class="room-image" src='${room[3]}'>/
+            <p class="price">${room[4]}</p>
+            <p class="room-view">View/${room[5]}</p>
+          </div>
+          </div>`;
+    });
+  }
+}
+
+function toggleCart() {
+  document.querySelector("#cart").classList.toggle("active");
+}
